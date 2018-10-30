09babfc2e73b16836dd210d0f88b663956f9f1e6
hklhai <hkhai@outlook.com>
2018-10-30 14:08:36 +0800

    文件管理初始化提交

---
+++ src/main/java/com/hxqh/filemanager/FileManagerApplication.java
@@ -0,0 +1,15 @@
+package com.hxqh.filemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Lin
+ */
+@SpringBootApplication
+public class FileManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileManagerApplication.class, args);
+    }
+}
---
+++ src/main/resources/boot.sql
@@ -0,0 +1,5 @@
+create database filemanager default character set utf8;
+
+use filemanager;
+
+
