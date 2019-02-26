3e9e82d05c073ca2519f9cfacc4a08cc699552de
hklhai <hkhai@outlook.com>
2019-02-26 08:07:57 +0800

    清除数据补充

--- src/main/resources/db/delete.sql
+++ src/main/resources/db/delete.sql
@@ -7,3 +7,8 @@ delete from tb_file_keyword;
 delete from tb_current_file_log;
 delete from tb_file;
 delete from tb_path where pathid > 5;
+
+# 删除文件夹下文件
+rm -rf /home/hadoop/doclinks/*
+mkdir -p /home/hadoop/doclinks/priavte
+mkdir -p /home/hadoop/doclinks/icon
